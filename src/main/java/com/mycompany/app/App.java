5857961a60c7e0b76962f6ed76cdc9973f7e1755
Harout Bezdjian <harout.bezdjian@xtractor.se>
2017-11-14 09:03:55 +0100

    adding ojdbc14 and Oracle sql connection test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,10 @@
 package com.mycompany.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,30 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        try{
+            //1. Load driver class
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            //2. Create the connection
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "javadeveloper", "javadeveloper");
+
+            //3. Create the statement object.
+            Statement statement = connection.createStatement();
+
+            //4. Execute query.
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM USERS");
+
+            while(resultSet.next()){
+                System.out.println("ID: " + resultSet.getInt("ID"));
+                System.out.println("FIRSTNAME: " + resultSet.getString("FIRSTNAME"));
+                System.out.println("LASTNAME: " + resultSet.getString("LASTNAME"));
+                System.out.println("EMAIL: " + resultSet.getString("EMAIL"));
+            }
+        }catch(Exception e){
+            System.out.println("- Exception ------ ");
+            e.printStackTrace();
+        }
     }
 }
