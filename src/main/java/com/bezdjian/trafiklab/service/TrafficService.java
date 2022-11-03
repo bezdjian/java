a34fb5485e8f52d51b5ab171c7a325e155aebcfe
bezdjian <bezdjian-370z@hotmail.com>
2022-11-03 12:37:37 +0100

    Convert to Webflux: Refactor TrafficService

--- src/main/java/com/bezdjian/trafiklab/service/TrafficService.java
+++ src/main/java/com/bezdjian/trafiklab/service/TrafficService.java
@@ -1,23 +1,14 @@
 package com.bezdjian.trafiklab.service;
 
 import com.bezdjian.trafiklab.client.TrafficLabClientService;
-import com.bezdjian.trafiklab.exception.ClientException;
-import com.bezdjian.trafiklab.model.BussStopPointsModel;
-import com.bezdjian.trafiklab.model.JourneyPatternPointOnLine;
-import com.bezdjian.trafiklab.model.JourneyPatternPointOnLineResults;
-import com.bezdjian.trafiklab.model.StopPoint;
-import com.bezdjian.trafiklab.model.TopTenListModel;
+import com.bezdjian.trafiklab.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -44,51 +35,40 @@ public class TrafficService {
      * @param lineNumber {@code Integer}
      * @return {@code List} of {@code BussStopPointsDTO}
      */
-    public List<BussStopPointsModel> findStopsByLineNumber(int lineNumber) throws ClientException {
+    public Mono<List<BussStopPointsModel>> findStopsByLineNumber(int lineNumber) {
 
-        JourneyPatternPointOnLine journeyPoint = clientService.getJourneyPoints();
+        Mono<List<JourneyPatternPointOnLineResults>> journeyPointsByLineNumber =
+            getJourneyPointsByLineNumber(lineNumber, clientService.getJourneyPoints());
 
-        /* select j.lineNumber, s.stopPointName " +
-                " from JourneyPointEntity AS j " +
-                " join StopPointEntity AS s on s.stopPointNumber = j.journeyPatternPointNumber" +
-                " where j.lineNumber = :lineNumber and j.directionCode = 1" +
-                " group by s.stopAreaNumber"; */
-        try {
-            List<JourneyPatternPointOnLineResults> journeyPointsByLineNumber =
-                getJourneyPointsByLineNumber(lineNumber, journeyPoint);
-
-            return getStopPoints(journeyPointsByLineNumber);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new ClientException(e.getMessage());
-        }
+        return getStopPoints(journeyPointsByLineNumber)
+            .doOnError(e -> log.error(e.getMessage()));
     }
 
-    private List<BussStopPointsModel> getStopPoints(
-        List<JourneyPatternPointOnLineResults> journeyPatternPointOnLineResults) throws
-        ClientException {
-        StopPoint stopPoint = clientService.getStopPoints();
-        List<BussStopPointsModel> stopPointsModels = new ArrayList<>();
-        stopPoint.getResponseData()
+    private Mono<List<BussStopPointsModel>> getStopPoints(
+        Mono<List<JourneyPatternPointOnLineResults>> journeyPatternPointOnLineResults) {
+
+        Mono<StopPoint> stopPoints = clientService.getStopPoints();
+
+        return Mono.zip(stopPoints, journeyPatternPointOnLineResults, (s, j) ->
+            s.getResponseData()
+                .getResult()
+                .stream()
+                .filter(s1 -> j.stream().anyMatch(j1 -> j1.getJourneyPatternPointNumber().equals(s1.getStopPointNumber())))
+                .map(s1 -> BussStopPointsModel.builder()
+                    .lineNumber(j.get(0).getLineNumber())
+                    .stopName(s1.getStopPointName())
+                    .build())
+                .collect(Collectors.toList()));
+    }
+
+    private Mono<List<JourneyPatternPointOnLineResults>> getJourneyPointsByLineNumber(int lineNumber,
+                                                                                      Mono<JourneyPatternPointOnLine> journeyPoint) {
+        return journeyPoint.map(jppo -> jppo.getResponseData()
             .getResult()
-            .stream()
-            .filter(s -> journeyPatternPointOnLineResults.stream()
-                .anyMatch(j -> j.getJourneyPatternPointNumber().equals(s.getStopPointNumber()))
-            )
-            .forEach(s -> stopPointsModels.add(BussStopPointsModel.builder()
-                .lineNumber(journeyPatternPointOnLineResults.get(0).getLineNumber())
-                .stopName(s.getStopPointName())
-                .build()));
-        return stopPointsModels;
-    }
-
-    private List<JourneyPatternPointOnLineResults> getJourneyPointsByLineNumber(int lineNumber,
-                                                                                JourneyPatternPointOnLine journeyPoint) {
-        return journeyPoint.getResponseData().getResult()
             .stream()
             .filter(s -> s.getLineNumber() == lineNumber)
             .filter(s -> s.getDirectionCode().equals("1"))
-            .collect(Collectors.toList());
+            .collect(Collectors.toList()));
     }
 
     /**
@@ -97,46 +77,52 @@ public class TrafficService {
      *
      * @return {@code Map} of String and Object
      */
-    public Map<String, Object> findLineWithMostStops() throws ClientException {
-        Map<String, Object> topTenAndStopNames = new HashMap<>();
-        HashMap<Integer, Long> lineNumbers = new HashMap<>(getAllLineNumbers());
-
+    public Mono<Map<String, Object>> findLineWithMostStops() {
         //Sort and retrieve top 10 line numbers with most stops.
-        List<Map.Entry<Integer, Long>> topTenList = getTopTenMostStops(lineNumbers);
-        List<TopTenListModel> topTenListModels = new ArrayList<>();
-        topTenList.forEach(t -> topTenListModels.add(TopTenListModel.builder()
-            .lineNumber(t.getKey())
-            .stopCount(t.getValue().toString())
-            .build()));
-        topTenAndStopNames.put("topTenList", topTenListModels);
-        //Get the stop names of the line number that has the most stops.
-        //getKey() is the line number we want to search.
-        if (!topTenList.isEmpty()) {
-            List<BussStopPointsModel> stopNames = findStopsByLineNumber(topTenList.get(0).getKey());
-            topTenAndStopNames.put("stopNames", stopNames);
+        return getTopTenMostStops(getAllLineNumbers())
+            .flatMap(topTenList -> {
+                Mono<Map<String, Object>> mapMono = Mono.empty();
+                Map<String, Object> topTenAndStopNames = new HashMap<>();
+                List<TopTenListModel> topTenListModels = new ArrayList<>();
 
-            log.info("***** Top 10 lines with most stops: " + topTenList);
-            log.info("***** Stop names of the most buss stop line: " + stopNames.size());
-            return topTenAndStopNames;
-        }
-        return Collections.emptyMap();
+                topTenList.forEach(t -> topTenListModels.add(TopTenListModel.builder()
+                    .lineNumber(t.getKey())
+                    .stopCount(t.getValue().toString())
+                    .build()));
+                topTenAndStopNames.put("topTenList", topTenListModels);
+                log.info("***** Top 10 lines with most stops: " + topTenList);
+
+                //Get the stop names of the line number that has the most stops.
+                //getKey() is the line number we want to search.
+                if (!topTenList.isEmpty()) {
+                    mapMono = findStopsByLineNumber(topTenList.get(0).getKey())
+                        .map(stopNames -> {
+                            topTenAndStopNames.put("stopNames", stopNames);
+                            log.info("***** Number of stop names of the most buss stop line: " + stopNames.size());
+                            return topTenAndStopNames;
+                        });
+                }
+                return mapMono;
+            });
     }
 
     /**
      * Private method that sorts and retrieves the top then lines that have the most stops
      *
-     * @param map {@code Map}
+     * @param lineNumbers {@code Map}
      * @return {@code List} of Entries
      */
-    private List<Map.Entry<Integer, Long>> getTopTenMostStops(HashMap<Integer, Long> map) {
-        Set<Map.Entry<Integer, Long>> set = map.entrySet();
-        List<Map.Entry<Integer, Long>> list = new ArrayList<>(set);
-        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-        //If more than 10, retrieve top ten.
-        if (list.size() > 10)
-            return list.subList(0, 10);
-        else
-            return list;
+    private Mono<List<Map.Entry<Integer, Long>>> getTopTenMostStops(Mono<Map<Integer, Long>> lineNumbers) {
+        return lineNumbers.map(m -> {
+            Set<Map.Entry<Integer, Long>> set = m.entrySet();
+            List<Map.Entry<Integer, Long>> list = new ArrayList<>(set);
+            list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+            //If more than 10, retrieve top ten.
+            if (list.size() > 10)
+                return list.subList(0, 10);
+            else
+                return list;
+        });
     }
 
     /**
@@ -144,13 +130,14 @@ public class TrafficService {
      *
      * @return {@code List} of JourneyPoint line numbers with stop counts.
      */
-    private Map<Integer, Long> getAllLineNumbers() throws ClientException {
-        return clientService.getJourneyPoints().getResponseData()
-            .getResult()
-            .stream()
-            .filter(s -> s.getDirectionCode().equals("1"))
-            .map(JourneyPatternPointOnLineResults::getLineNumber)
-            //<lineNumber, number of stops>
-            .collect(groupingBy(Integer::intValue, Collectors.counting()));
+    private Mono<Map<Integer, Long>> getAllLineNumbers() {
+        return clientService.getJourneyPoints()
+            .map(jp -> jp.getResponseData()
+                .getResult()
+                .stream()
+                .filter(s -> s.getDirectionCode().equals("1"))
+                .map(JourneyPatternPointOnLineResults::getLineNumber)
+                //<lineNumber, number of stops>
+                .collect(groupingBy(Integer::intValue, Collectors.counting())));
     }
 }
--- src/test/java/com/bezdjian/trafiklab/service/TrafficServiceTest.java
+++ src/test/java/com/bezdjian/trafiklab/service/TrafficServiceTest.java
@@ -8,13 +8,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.initMocks;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 class TrafficServiceTest {
@@ -27,20 +28,22 @@ class TrafficServiceTest {
     @BeforeEach
     void setUp() throws ClientException {
         openMocks(this);
-        when(client.getJourneyPoints()).thenReturn(TestUtils.getJourneyPatternPointOnLine());
-        when(client.getStopPoints()).thenReturn(TestUtils.createStopPoints());
+        when(client.getJourneyPoints()).thenReturn(Mono.just(TestUtils.getJourneyPatternPointOnLine()));
+        when(client.getStopPoints()).thenReturn(Mono.just(TestUtils.createStopPoints()));
     }
 
     @Test
     void findStopsByLineNumber() throws ClientException {
-        List<BussStopPointsModel> list = trafficService.findStopsByLineNumber(172);
-        System.out.println("List: " + list.toString());
+        List<BussStopPointsModel> list = trafficService.findStopsByLineNumber(172).block();
+        assertNotNull(list);
+        System.out.println("List: " + list);
         assertEquals(2, list.size());
     }
 
     @Test
     void findLineWithMostStopsMoreThanTen() throws ClientException {
-        Map<String, Object> map = trafficService.findLineWithMostStops();
+        Map<String, Object> map = trafficService.findLineWithMostStops().block();
+        assertNotNull(map);
         assertEquals(2, map.size());
     }
 }
