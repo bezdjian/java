f414b446303c8ff07895a31e2701ae58ebc35a59
bezdjian <bezdjian-370z@hotmail.com>
2022-12-19 17:10:31 +0100

    Fix keys for divs

--- src/components/BussStops/TopTenStopNames.js
+++ src/components/BussStops/TopTenStopNames.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const TopTenStopNames = ({stopNames}) => {
   return (
-    <div key='top-names-card'>
-      <div key='top-names-card-body' className="card-body">
-        <h5 key='linenumber' className="card-title">The line number here..</h5>
+    <div>
+      <div className="card-body">
+        <h5 id='linenumber' className="card-title">The line number here..</h5>
         {stopNames.map((stops) => (
           <li key={stops.stopName} className="card-title">{stops.stopName}</li>
         ))}
