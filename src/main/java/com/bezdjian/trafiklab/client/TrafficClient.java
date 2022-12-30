7c172e47ed1f8fa417c1529cdc4051ca182c38ec
bezdjian <bezdjian-370z@hotmail.com>
2022-12-30 19:34:50 +0100

    Refactor WebClient, initialize it in constructor.

--- src/main/java/com/bezdjian/trafiklab/client/TrafficClient.java
+++ src/main/java/com/bezdjian/trafiklab/client/TrafficClient.java
@@ -10,42 +10,39 @@ import reactor.core.publisher.Mono;
 @Component
 public class TrafficClient {
 
-    private final String baseUrl;
-    private final String key;
+  private final String key;
+  private final WebClient client;
 
-    public TrafficClient(@Value("${trafiklab.api.url}") String baseUrl,
-                         @Value("${trafiklab.api.key}") String key) {
-        this.baseUrl = baseUrl;
-        this.key = key;
-    }
+  public TrafficClient(@Value("${trafiklab.api.url}") String baseUrl,
+                       @Value("${trafiklab.api.key}") String key) {
+    this.key = key;
+    this.client = WebClient.builder()
+      .baseUrl(baseUrl)
+      .codecs(codec -> codec.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
+      .build();
+  }
 
-    public Mono<StopPoint> getStopPoints() {
-        WebClient client = WebClient.builder()
-            .baseUrl(baseUrl + "/LineData.json?model=stop&key=" + key)
-            .codecs(codec -> codec.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
-            .build();
+  public Mono<StopPoint> getStopPoints() {
+    return this.client
+      .get()
+      .uri("/LineData.json?model=stop&key=" + key)
+      .exchangeToMono(response -> {
+        if (response.statusCode().isError()) {
+          return response.createException().flatMap(Mono::error);
+        }
+        return response.bodyToMono(StopPoint.class);
+      });
+  }
 
-        return client.get()
-            .exchangeToMono(response -> {
-                if (response.statusCode().isError()) {
-                    return response.createException().flatMap(Mono::error);
-                }
-                return response.bodyToMono(StopPoint.class);
-            });
-    }
-
-    public Mono<JourneyPatternPointOnLine> getJourneyPatternPointOnLine() {
-        WebClient client = WebClient.builder()
-            .baseUrl(baseUrl + "/LineData.json?model=jour&key=" + key)
-            .codecs(codec -> codec.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
-            .build();
-
-        return client.get()
-            .exchangeToMono(response -> {
-                if (response.statusCode().isError()) {
-                    return response.createException().flatMap(Mono::error);
-                }
-                return response.bodyToMono(JourneyPatternPointOnLine.class);
-            });
-    }
+  public Mono<JourneyPatternPointOnLine> getJourneyPatternPointOnLine() {
+    return this.client
+      .get()
+      .uri("/LineData.json?model=jour&key=" + key)
+      .exchangeToMono(response -> {
+        if (response.statusCode().isError()) {
+          return response.createException().flatMap(Mono::error);
+        }
+        return response.bodyToMono(JourneyPatternPointOnLine.class);
+      });
+  }
 }
