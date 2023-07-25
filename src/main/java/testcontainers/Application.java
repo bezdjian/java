1a2db288251932aa8b4bb1219c583bf3ae099b26
Harout Bezdjian <harout.bezdjian@wirelesscar.com>
2023-07-25 10:29:23 +0200

    Move restTemplate bean to Beans class

--- src/main/java/testcontainers/Application.java
+++ src/main/java/testcontainers/Application.java
@@ -2,8 +2,6 @@ package testcontainers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
@@ -12,10 +10,5 @@ public class Application {
 
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
-  }
-
-  @Bean
-  public RestTemplate restTemplate() {
-    return new RestTemplate();
   }
 }
--- src/main/java/testcontainers/config/Beans.java
+++ src/main/java/testcontainers/config/Beans.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.SnsClientBuilder;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -17,6 +18,11 @@ public class Beans {
 
   @Value("${localstack.sns.endpoint}")
   private String snsEndpoint;
+
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
   @Bean
   public SnsClient snsClient() {
