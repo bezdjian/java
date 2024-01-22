7bb6c4e08aa65197a5e0f3b2e6d908799b359dc8
Harout Bezdjian <harout.bezdjian@wirelesscar.com>
2024-01-22 12:40:29 +0100

    Add reactiveMongoTemplate bean

--- src/main/java/testcontainers/config/Beans.java
+++ src/main/java/testcontainers/config/Beans.java
@@ -1,9 +1,15 @@
 package testcontainers.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
+import org.bson.UuidRepresentation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -19,56 +25,76 @@ import java.net.URI;
 @Configuration
 public class Beans {
 
-    public static final String DEFAULT_URL = "default";
-    @Value("${localstack.url}")
-    private String localStackUrl;
-    @Value("${localstack.s3.url}")
-    private String s3LocalStackUrl;
+  public static final String DEFAULT_URL = "default";
+  @Value("${localstack.url}")
+  private String localStackUrl;
+  @Value("${localstack.s3.url}")
+  private String s3LocalStackUrl;
+  @Value("${spring.data.mongodb.uri}")
+  private String databaseUri;
+  @Value("${spring.data.mongodb.database}")
+  private String databaseName;
 
-    @Bean
-    public WebClient webClient() {
-        return WebClient.create();
+  @Bean
+  public WebClient webClient() {
+    return WebClient.create();
+  }
+
+  @Bean
+  public MongoClient mongoClient() {
+    ConnectionString connectionString = new ConnectionString(databaseUri);
+    MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+      .applyConnectionString(connectionString)
+      .uuidRepresentation(UuidRepresentation.STANDARD)
+      .build();
+
+    return MongoClients.create(mongoClientSettings);
+  }
+
+  @Bean
+  public ReactiveMongoTemplate reactiveMongoTemplate() {
+    return new ReactiveMongoTemplate(mongoClient(), databaseName);
+  }
+
+  @Bean
+  public SnsClient snsClient() {
+    SnsClientBuilder snsClient = SnsClient.builder();
+    if (!localStackUrl.equals(DEFAULT_URL)) {
+      log.info("Using localstack SNS client with endpoint: {}", localStackUrl);
+      snsClient.endpointOverride(URI.create(localStackUrl))
+        .region(Region.EU_WEST_1)
+        .build();
     }
 
-    @Bean
-    public SnsClient snsClient() {
-        SnsClientBuilder snsClient = SnsClient.builder();
-        if (!localStackUrl.equals(DEFAULT_URL)) {
-            log.info("Using localstack SNS client with endpoint: {}", localStackUrl);
-            snsClient.endpointOverride(URI.create(localStackUrl))
-                    .region(Region.EU_WEST_1)
-                    .build();
-        }
+    log.info("Using default SNS client");
+    return snsClient.build();
 
-        log.info("Using default SNS client");
-        return snsClient.build();
+  }
 
+  @Bean
+  public SqsClient sqsClient() {
+    SqsClientBuilder sqsClient = SqsClient.builder();
+    if (!localStackUrl.equals(DEFAULT_URL)) {
+      log.info("Using localstack SQS client with endpoint: {}", localStackUrl);
+      return sqsClient.endpointOverride(URI.create(localStackUrl))
+        .region(Region.EU_WEST_1)
+        .build();
     }
+    log.info("Using default SQS client");
+    return sqsClient.build();
 
-    @Bean
-    public SqsClient sqsClient() {
-        SqsClientBuilder sqsClient = SqsClient.builder();
-        if (!localStackUrl.equals(DEFAULT_URL)) {
-            log.info("Using localstack SQS client with endpoint: {}", localStackUrl);
-            return sqsClient.endpointOverride(URI.create(localStackUrl))
-                    .region(Region.EU_WEST_1)
-                    .build();
-        }
-        log.info("Using default SQS client");
-        return sqsClient.build();
+  }
 
+  @Bean
+  public S3Client s3Client() {
+    S3ClientBuilder s3Client = S3Client.builder();
+    if (!s3LocalStackUrl.equals(DEFAULT_URL)) {
+      log.info("Using localstack S3 client with endpoint: {}", s3LocalStackUrl);
+      s3Client.endpointOverride(URI.create(s3LocalStackUrl))
+        .region(Region.EU_WEST_1)
+        .build();
     }
-
-    @Bean
-    public S3Client s3Client() {
-        S3ClientBuilder s3Client = S3Client.builder();
-        if (!s3LocalStackUrl.equals(DEFAULT_URL)) {
-            log.info("Using localstack S3 client with endpoint: {}", s3LocalStackUrl);
-            s3Client.endpointOverride(URI.create(s3LocalStackUrl))
-                    .region(Region.EU_WEST_1)
-                    .build();
-        }
-        log.info("Using default S3 client");
-        return s3Client.build();
-    }
+    log.info("Using default S3 client");
+    return s3Client.build();
+  }
 }
